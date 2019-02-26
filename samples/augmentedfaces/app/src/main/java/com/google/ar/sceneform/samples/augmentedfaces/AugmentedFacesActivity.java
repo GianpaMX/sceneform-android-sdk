b317d56cee0d04a945e658b67e9a0f3496055734
Juan Pablo "Gianpa" Villa <juan.villa@wizeline.com>
2019-02-26 11:00:21 -0600

    Unable to download gltf files

--- samples/augmentedfaces/app/src/main/java/com/google/ar/sceneform/samples/augmentedfaces/AugmentedFacesActivity.java
+++ samples/augmentedfaces/app/src/main/java/com/google/ar/sceneform/samples/augmentedfaces/AugmentedFacesActivity.java
@@ -18,9 +18,10 @@ package com.google.ar.sceneform.samples.augmentedfaces;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.widget.Toast;
 import com.google.ar.core.ArCoreApk;
 import com.google.ar.core.AugmentedFace;
@@ -28,14 +29,19 @@ import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.assets.RenderableSource;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.rendering.Texture;
 import com.google.ar.sceneform.ux.AugmentedFaceNode;
+import com.google.common.io.Files;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * This is an example activity that uses the Sceneform UX package to make common Augmented Faces
@@ -43,6 +49,7 @@ import java.util.Map;
  */
 public class AugmentedFacesActivity extends AppCompatActivity {
   private static final String TAG = AugmentedFacesActivity.class.getSimpleName();
+  private static final String GLTF_ASSET = "http://192.168.43.63:8000/SaltMarty.gltf";
 
   private static final double MIN_OPENGL_VERSION = 3.0;
 
@@ -67,17 +74,38 @@ public class AugmentedFacesActivity extends AppCompatActivity {
     setContentView(R.layout.activity_face_mesh);
     arFragment = (FaceArFragment) getSupportFragmentManager().findFragmentById(R.id.face_fragment);
 
+    Files.createTempDir();
+
+    RenderableSource renderableSource = RenderableSource.builder().setSource(
+            this,
+            Uri.parse(GLTF_ASSET),
+            RenderableSource.SourceType.GLTF2)
+            .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+            .build();
+
     // Load the face regions renderable.
     // This is a skinned model that renders 3D objects mapped to the regions of the augmented face.
     ModelRenderable.builder()
-        .setSource(this, R.raw.fox_face)
+        .setSource(this, renderableSource)
+        .setRegistryId(GLTF_ASSET)
         .build()
         .thenAccept(
             modelRenderable -> {
               faceRegionsRenderable = modelRenderable;
               modelRenderable.setShadowCaster(false);
               modelRenderable.setShadowReceiver(false);
-            });
+            })
+            .exceptionally(
+                    throwable -> {
+                      Log.e(TAG, "ModelRenderable", throwable);
+                      Toast toast =
+                              Toast.makeText(this, "Unable to load renderable " +
+                                      GLTF_ASSET, Toast.LENGTH_LONG);
+                      toast.setGravity(Gravity.CENTER, 0, 0);
+                      toast.show();
+                      return null;
+                    });
+    ;
 
     // Load the face mesh texture.
     Texture.builder()
